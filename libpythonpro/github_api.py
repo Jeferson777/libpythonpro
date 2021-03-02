32a3933c629be55d511829d16daf8e97887e590a
Jeferson Soaris <g7harmonic@outlook.com>
2021-03-02 03:43:10 -0300

    #Teste: Instalando a lib request e criando um módulo para teste da api do github.

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -1,13 +1,16 @@
 import requests
 
-
 def buscar_avatar(usuario):
     """
-    Busca o avatar de um usuário no Github
-
+    Buscar o avatar de um usuário no Github
     :param usuario: str com o nome de usuário no github
     :return: str com o link do avatar
     """
     url = f'https://api.github.com/users/{usuario}'
-    resp = requests.get(url)
-    return resp.json()['avatar_url']
+    resp = requests.get(url) # Requisição de URL na variável url.
+    return resp.json()['avatar_url'] #Chave contendo a ulr "https://avatars.githubusercontent.com/u/61337568?v=4"
+
+#Confirmando a requisição.
+#Usando a propriedade __name__ devido importação de módulos:
+if __name__ == '__main__':
+    print(buscar_avatar('jeferson777')) #Usuário github
